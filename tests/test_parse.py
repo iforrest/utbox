dd5577a00fde2e642237a6ae3c655035da611056
Daniel Federschmidt <dfederschmidt@splunk.com>
2022-06-01 12:17:16 +0200

    create logfile

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,6 +1,11 @@
 import sys
 import os
 import unittest
+from pathlib import Path
+
+log_path = Path(os.environ["SPLUNK_HOME"] + "/var/log/splunk")
+log_path.mkdir(parents=True, exist_ok=True)
+(log_path / Path("utbox.log")).write_text("")
 
 path_to_add = os.path.abspath(os.path.join(__file__, '..','..','utbox', 'bin'))
 sys.path.append(path_to_add)
